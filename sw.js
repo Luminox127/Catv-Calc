3e6f4bf793bc7c039fb01ab20a97041c5af69a1f
Luminox <fg73835@gmail.com>
2026-02-06 18:21:11 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "catv-terminal-v1";
+const CACHE_NAME = "catv-terminal-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -25,6 +25,7 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
+
 
 
 
